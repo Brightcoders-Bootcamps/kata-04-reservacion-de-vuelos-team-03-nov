90fd0b30f47e60e16e96f9a8e1014992cdf8133c
mariam-rivera <riverahmariam@gmail.com>
2021-01-19 12:02:20 -0600

    Snapshoot testing working

---
+++ ReservacionDeVuelos/__tests__/SignInScreen-test.js
@@ -0,0 +1,9 @@
+// __tests__/Intro-test.js
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SignInScreen from '../src/screens/SignInScreen';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<SignInScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
