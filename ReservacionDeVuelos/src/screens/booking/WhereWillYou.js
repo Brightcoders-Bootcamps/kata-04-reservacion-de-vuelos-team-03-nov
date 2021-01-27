0dfff1c5b20fc925e4f8dea091554faa5fa918c7
mariam-rivera <riverahmariam@gmail.com>
2021-01-27 11:28:12 -0600

    Back arrow added

--- ReservacionDeVuelos/src/screens/booking/WhereWillYou.js
+++ ReservacionDeVuelos/src/screens/booking/WhereWillYou.js
@@ -1,41 +1,56 @@
 import React from 'react';
-import {View, StyleSheet, Text } from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import FormButton from '../../components/FormButton';
 import contentText from './../../utils/Constants';
 import BookingInput from './../../components/BookingInput';
+import IconBack from 'react-native-vector-icons/Ionicons';
+import colors from './../../utils/Colors';
 
 const whereWillYou = ({navigation}) => {
-    return(
-        <View style={styles.container}>
-            <Text style={styles.title}>{contentText.whereWillYou}</Text>
-            <BookingInput
-            placeHolderText="Select location"
-            />
-            <FormButton
-             buttonTitle="Next"
-             backgroundColor={colors.gray}
-             color={colors.white}
-             onPress={() => navigation.navigate('TravelDate')}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <IconBack
+          name="chevron-back"
+          style={styles.backIcon}
+          onPress={() => navigation.navigate('Home')}
+        />
+      </TouchableOpacity>
+      <Text style={styles.title}>{contentText.whereWillYou}</Text>
+      <BookingInput placeHolderText="Select location" />
+      <FormButton
+        buttonTitle="Next"
+        backgroundColor={colors.gray}
+        color={colors.white}
+        onPress={() => navigation.navigate('TravelDate')}
+      />
+    </View>
+  );
 };
 
 export default whereWillYou;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        padding: 5
-    },
-    title: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        marginTop: 80,
-        marginRight: 35,
-        marginLeft: 20,
-        marginBottom: 10
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 5,
+    backgroundColor: colors.white,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginRight: 35,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  backIcon: {
+    marginTop: 5,
+    fontSize: 48,
+    color: colors.blue,
+    backgroundColor: colors.white,
+    marginRight: 300,
+  },
 });
